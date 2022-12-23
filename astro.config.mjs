d2b7375d7ac4a6cc2761075ce054b23ed6a36a00
Felix Zeller <felixazeller@gmail.com>
2022-12-23 12:05:07 -0500

    Basic index and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), tailwind()]
 });
