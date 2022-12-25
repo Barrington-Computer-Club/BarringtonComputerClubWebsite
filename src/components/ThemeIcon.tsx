4f1a2ed4ddab933933ceb7c0d6e7c4572c2cc109
Felix Zeller <felixazeller@gmail.com>
2022-12-24 19:21:28 -0500

    Theme Icon TSX TODO: Fix spacing

---
+++ src/components/ThemeIcon.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import "@theme-toggles/react/css/Within.css"
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
+
+const ThemeIcon = () => {
+  const [isDarkMode, setDarkMode] = React.useState(false);
+
+  const toggleDarkMode = (checked: boolean) => {
+    setDarkMode(checked);
+  };
+
+  document.documentElement.classList.toggle('dark', isDarkMode);
+
+  return (
+    <DarkModeSwitch
+      style={{ position: 'absolute', top: '1rem', right: '1rem' }}
+      checked={isDarkMode}
+      onChange={toggleDarkMode}
+      size={50}
+    />
+  );
+};
+
+export default ThemeIcon
