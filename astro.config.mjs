684456d59ed2932c4e76894db4323b3be275662a
Felix Zeller <felixazeller@gmail.com>
2023-01-29 22:46:24 -0500

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,5 +18,6 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react(), tailwind({
     config: { applyBaseStyles: false }
-  }), image()]
+  }), image()],
+  output: 'server'
 });
