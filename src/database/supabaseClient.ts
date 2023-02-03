bf1f3cdff685256402357adf1d806ccc867dc93b
Felix Zeller <felixazeller@gmail.com>
2023-02-03 17:42:27 -0500

    FIxing databse keys

--- src/database/supabaseClient.ts
+++ src/database/supabaseClient.ts
@@ -2,9 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 import type { Database } from '../../lib/database.types'
 
-// TODO: FIx this
-const supabaseurl = "http://localhost:54321"
-const supabasekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
+const supabaseurl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabasekey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient<Database>(supabaseurl, supabasekey);
 
