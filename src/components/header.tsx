391b61dd742a05abbf2e924a9d291fc7eba74365
Siddharth Gupta <109366063+Windshield-Viper@users.noreply.github.com>
2023-10-04 10:57:17 -0400

    fix navbar, change git icon, add 404 page, and more

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -21,63 +21,41 @@ import { Button } from "@/components/ui/button"
 export default function Header() {
   return <nav className="absolute top-0 right-0 left-0 flex flex-row gap-2 px-20 h-20 items-center">
 
-    <a rel="prefetch" href="/"><h3>Barrington Computer Club</h3></a>
+    <a rel="prefetch" href="/"><h3>Barrington Computer Club &nbsp;</h3></a>
 
     <NavigationMenu>
 
       <NavigationMenuList>
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Upcoming Events</NavigationMenuTrigger>
-          <NavigationMenuContent className="">
-            <div className="p-6 grid grid-cols-[1fr_2fr] gap-4 w-[400px]">
-              <div className="flex flex-col gap-2">
-                <p className="text-xs">The upcoming events held during Computer Club meetings</p>
-                <NavigationMenuLink><a rel="prefetch" href="/events"><Button variant="outline">View All</Button></a></NavigationMenuLink>
-              </div>
-              <div className="flex flex-col gap-2">
-                <NavigationMenuLink><NavigationLink title="AI" description="Learn the basics of AI" /></NavigationMenuLink>
-                <NavigationMenuLink><NavigationLink title="Databases" description="Learn how apps use databases and the basics of SQL" /></NavigationMenuLink>
-                <NavigationMenuLink><NavigationLink title="Functional Programming" description="Learn the basics of functional programming in Rust" /></NavigationMenuLink>
-              </div>
-            </div>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Content</NavigationMenuTrigger>
-          <NavigationMenuContent className="">
-            <div className="p-6 w-[450px]">
-              <p className="mb-3">Content Created by Members of Computer Club</p>
-              <div className="flex flex-col gap-2">
-                <NavigationMenuLink><NavigationLink title="Archived Presentations" description="Content corresponding to presentations given during club meetings" href="/presentations" /></NavigationMenuLink>
-                <NavigationMenuLink><NavigationLink title="Projects" description="Exhibition of projects created by club members" href='/projects' /></NavigationMenuLink>
-                <NavigationMenuLink><NavigationLink title="Blog Posts" description="Blog posts created by club members" href='/posts' /></NavigationMenuLink>
-              </div>
-            </div>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
+      {/* Such great web dev */}
+      &nbsp;
+      &nbsp;
+      &nbsp;
+        <NavigationMenuLink><NavigationLink title="Upcoming Events" href="/events" /></NavigationMenuLink>
+        <NavigationMenuLink><NavigationLink title="Community Blog Posts" href="/posts" /></NavigationMenuLink>
+        <NavigationMenuLink><NavigationLink title="Archived Presentations" href="/presentations" /></NavigationMenuLink>
+        <NavigationMenuLink><NavigationLink title="CS Project Gallery" href="/projects" /></NavigationMenuLink>
 
 
       </NavigationMenuList>
 
 
-
+      
     </NavigationMenu>
-
+    
 
     <div className="ml-auto">
       <TooltipProvider>
         <Tooltip>
-          <TooltipTrigger><a rel="prefetch" href="https://github.com/Feel-ix-343/BarringtonComputerClubWebsite"><img width="50" src="https://img.icons8.com/3d-fluency/94/github.png" alt="github" /></a></TooltipTrigger>
+          <TooltipTrigger><a rel="prefetch" href="https://github.com/Feel-ix-343/BarringtonComputerClubWebsite"><img width="70" src="public/icons8-github-128.png" alt="github" /></a></TooltipTrigger>
           <TooltipContent>
             <p>View Source Code</p>
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
+      
     </div>
 
+  
   </nav>
 }
 
@@ -87,3 +65,4 @@ function NavigationLink(props: {title: string, description?: string, href?: stri
     <p className="text-xs">{props.description}</p>
   </a>
 }
+
